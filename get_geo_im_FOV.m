1a6fb76c92e8165abc78479887fa5644f334cfff
Yuki Itoh <yukiitohand@gmail.com>
2020-02-29 23:46:24 -0500

    saving is added

--- get_geo_im_FOV.m
+++ get_geo_im_FOV.m
@@ -1,4 +1,4 @@
-function [geo_im_FOV_mask] = get_geo_im_FOV(basename_dem,dpath_dem,rover_nav_coord,cmmdl,im_size)
+function [geo_im_FOV_mask] = get_geo_im_FOV(basename_dem,dpath_dem,rover_nav_coord,cmmdl,im_size,varargin)
 % [geo_im_FOV_mask] = get_geo_im_FOV(basename_dem,dpath_dem,rover_nav,cmmdl,im_size)
 %   evaluate FOV of an image on an ortho-georeferenced image using a
 %   georeferenced DEM image.
@@ -22,6 +22,28 @@ function [geo_im_FOV_mask] = get_geo_im_FOV(basename_dem,dpath_dem,rover_nav_coo
 %  OUTPUTS
 %    geo_im_FOV_mask: boolean image, true if in the FOV, false otherwise.
 
+is_gpu = false;
+precision = 'double';
+if (rem(length(varargin),2)==1)
+    error('Optional parameters should always go by pairs');
+else
+    for i=1:2:(length(varargin)-1)
+        switch upper(varargin{i})
+            case 'GPU'
+                is_gpu = varargin{i+1};
+            case 'PRECISION'
+                precision = varargin{i+1};
+            otherwise
+                error('Unrecognized option: %s',varargin{i});
+        end
+    end
+end
+
+if is_gpu
+    gpu_varargin = {'gpuArray'};
+else
+    gpu_varargin = {};
+end
 
 cmmdl_C = cmmdl.C;
 cmmdl_A = cmmdl.A;
@@ -29,6 +51,13 @@ cmmdl_H = cmmdl.H;
 cmmdl_V = cmmdl.V;
 L_im = im_size(1);
 S_im = im_size(2);
+
+if is_gpu
+    cmmdl_C = gpuArray(cmmdl_C);
+    cmmdl_A = gpuArray(cmmdl_A);
+    cmmdl_H = gpuArray(cmmdl_H);
+    cmmdl_V = gpuArray(cmmdl_V);
+end
 
 % Get rover coordinate
 rov_northing  = rover_nav_coord.NORTHING;
@@ -41,18 +70,46 @@ rov_rot_mat = get_rot_mat(rov_roll,rov_pitch, rov_yaw);
 
 hdr_dem = envihdrreadx(joinPath(dpath_dem,[basename_dem '.hdr']));
 L_geo = hdr_dem.lines; S_geo = hdr_dem.samples;
+geo_northing   = reshape(hdr_dem.y,L_geo,1);
+geol_easting    = reshape(hdr_dem.x,1,S_geo);
+
+if is_gpu
+    rov_rot_mat = gpuArray(rov_rot_mat);
+    geo_northing = gpuArray(geo_northing);
+    geol_easting = gpuArray(geol_easting);
+end
+
+switch lower(precision)
+    case 'double'
+        geo_northing = double(geo_northing);
+        geol_easting = double(geol_easting);
+    case 'single'
+        geo_northing = single(geo_northing);
+        geol_easting = single(geol_easting);
+end
 
 % Perform line by line operation
-geo_im_FOV_mask = false(L_geo,S_geo);
+geo_im_FOV_mask = false(L_geo,S_geo,gpu_varargin{:});
 
+% Open image to store coordinate in the image space.
+hdr_out = hdr_dem;
+hdr_out.interleave = 'bil';
+hdr_out.bands = 2;
+hdr_out.band_names = {'imx','imy'};
+out_file_path = joinPath(dpath_dem, [basename_dem '_imxy.img']);
+
+envihdrwritex(hdr_out,joinPath(dpath_dem, [basename_dem '_imxy.hdr']));
 for l = 1:L_geo
     %==========================================================================
     % projection of ground reference coordinates to ROVER_NAV coordinate
     %==========================================================================
     % get
-    geol_northing   = repmat(hdr_dem.y(l),[1,S_geo]);
-    geol_easting    = hdr_dem.x;
+    geol_northing   = repmat(geo_northing(l),[1,S_geo]);
+    % geol_easting    = hdr_dem.x;
     geol_elevation = lazyEnviReadl(joinPath(dpath_dem,[basename_dem '.img']),hdr_dem,l);
+    if is_gpu
+        geol_elevation = gpuArray(geol_elevation);
+    end
     
     % ---------------------------------------------------------------------
     % northing easting to rover coordinate with no rotation
@@ -69,12 +126,19 @@ for l = 1:L_geo
     %==========================================================================
     right_dir = (cmmdl_A * (geol_rov-cmmdl_C')) > -1; % safeguard
 
-    geol_im = [cmmdl_H;cmmdl_V] * (geol_rov-cmmdl_C') ./ (cmmdl_A * (geol_rov-cmmdl_C')); % 2 x S_geo
+    geol_im = cat(1,cmmdl_H,cmmdl_V) * (geol_rov-cmmdl_C') ./ (cmmdl_A * (geol_rov-cmmdl_C')); % 2 x S_geo
     geol_im_FOV = and(and(all(geol_im>-200,1),geol_im(1,:)<S_im+200),geol_im(2,:)<L_im+200);
 
     geol_im_FOV_mask = and(right_dir,geol_im_FOV);
     
     geo_im_FOV_mask(l,:) = geol_im_FOV_mask;
+    
+    if is_gpu
+        geol_im = gather(geol_im);
+    end
+    lazyEnviWritel( out_file_path,single(geol_im),hdr_out,l,'a');
+    
 end
+
 
 end
